a02c53ae04283b9a8a0d7876ab2d110f19458a7d
Petrik van der Velde <petrikvandervelde@gmail.com>
2023-08-26 15:26:14 +1200

    Fix the setup.py file

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
+from glob import glob
+import os
 from setuptools import setup
+
 
 package_name = 'zinger_controller_test_nodes'
 
@@ -10,6 +13,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Include all launch files.
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        # Include all the config files
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
