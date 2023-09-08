647d1163bd6049e3bbde8f91bea74e94be6ee334
Petrik van der Velde <petrikvandervelde@gmail.com>
2023-09-09 01:22:17 +1200

    Update the drive controller

--- zinger_controller_test_nodes/drive_controller.py
+++ zinger_controller_test_nodes/drive_controller.py
@@ -10,10 +10,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import List
 import rclpy
 from rclpy.node import Node
 from builtin_interfaces.msg import Duration
 
+from std_msgs.msg import Float64MultiArray
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from sensor_msgs.msg import JointState
 
@@ -36,6 +38,8 @@ class DriveController(Node):
         vel_names = self.get_parameter("vel_names").value
         acc_names = self.get_parameter("acc_names").value
         self.joints = self.get_parameter("joints").value
+
+        self.segment_duration_in_seconds = 1.0
 
         if self.joints is None or len(self.joints) == 0:
             raise Exception('"joints" parameter is not set!')
@@ -76,17 +80,29 @@ class DriveController(Node):
                 float_accelerations.append(float(value))
             self.accelerations.append(float_accelerations)
 
+        self.profile_duration = self.segment_duration_in_seconds * len(self.velocities)
+        self.get_logger().info(
+            'Profile duration set to: {} s'.format(self.profile_duration)
+        )
+
+        self.profile_and_wait_duration = self.profile_duration + wait_sec_between_profiles
+        self.get_logger().info(
+            'Profile and wait duration set to: {} s'.format(self.profile_and_wait_duration)
+        )
+
         publish_topic = "/" + controller_name + "/" + "commands"
 
         self.get_logger().info(
-            'Publishing {} goals on topic "{}" every {} s'.format(
-                len(vel_names), publish_topic, wait_sec_between_profiles
+            'Publishing {} goals on topic "{}" at {} Hz'.format(
+                len(pos_names), publish_topic, publishing_rate_in_hz
             )
         )
 
-        self.publisher_ = self.create_publisher(JointTrajectory, publish_topic, 1)
+        self.publisher_ = self.create_publisher(Float64MultiArray, publish_topic, 1)
+
+        self.sequence_start_time = self.get_clock().now()
         self.timer = self.create_timer(
-            wait_sec_between_profiles,
+            1.0 / publishing_rate_in_hz,
             self.timer_callback,
             callback_group=None,
             clock=self.get_clock())
@@ -97,23 +113,78 @@ class DriveController(Node):
             'Timer callback called ..'
         )
 
-        traj = JointTrajectory()
-        traj.joint_names = self.joints
-        #traj.header.stamp = self.get_clock().now().to_msg()
-        for i in range(len(self.velocities)):
-            point = JointTrajectoryPoint()
-            point.velocities = self.velocities[i]
-            point.accelerations = self.accelerations[i]
-            time = i * 1 + 1
-            point.time_from_start = Duration(sec=time)
-
-            traj.points.append(point)
-
+        current_time = self.get_clock().now()
+        trajectory_running_duration: Duration = current_time - self.sequence_start_time
         self.get_logger().info(
-            'Publishing movement command {} '.format(traj)
+            'Current trajectory duration {} s. Based on current time {} and sequence start time {}'.format(
+                trajectory_running_duration,
+                current_time,
+                self.sequence_start_time
+            )
         )
 
-        self.publisher_.publish(traj)
+        running_duration_as_float: float = trajectory_running_duration.nanoseconds * 1e-9
+        self.get_logger().info(
+            'Current trajectory duration {} s'.format(running_duration_as_float)
+        )
+
+        if running_duration_as_float > self.profile_and_wait_duration:
+            self.sequence_start_time = self.get_clock().now()
+
+            self.get_logger().info(
+                'Trajectory finished resetting start time to: {}'.format(self.sequence_start_time)
+            )
+            return
+
+        if running_duration_as_float > self.profile_duration:
+            self.get_logger().info(
+                'Trajectory completed waiting for restart time. Current duration {} s. Desired total time {}'.format(running_duration_as_float, self.profile_duration)
+            )
+            return
+
+        self.get_logger().info(
+            'Calculating next step in profile at time {} s'.format(running_duration_as_float)
+        )
+
+        lower_bound_of_profile_section = int(running_duration_as_float)
+        upper_bound_of_profile_section = lower_bound_of_profile_section + 1
+
+        time_fraction = (running_duration_as_float - lower_bound_of_profile_section) / self.segment_duration_in_seconds
+
+        if (lower_bound_of_profile_section < 0):
+            self.get_logger().info(
+                'Starting profile index out of range. Index is {}. Ignoring'.format(lower_bound_of_profile_section)
+            )
+            return
+
+        if (upper_bound_of_profile_section >= len(self.velocities)):
+            self.get_logger().info(
+                'Ending profile index out of range. Index is {}. Ignoring'.format(upper_bound_of_profile_section)
+            )
+            return
+
+        profile_start_values = self.velocities[lower_bound_of_profile_section]
+        profile_end_values = self.velocities[upper_bound_of_profile_section]
+
+        # Find the section we're publishing, assuming that each section takes 1 second
+        # 0 - 1 -> first
+
+        values: List[float] = []
+        for i in range(len(profile_start_values)):
+            start = profile_start_values[i]
+            end = profile_end_values[i]
+
+            value = (end - start) * time_fraction + start
+            values.append(value)
+
+        msg = Float64MultiArray()
+        msg.data = values
+
+        self.get_logger().info(
+            'Publishing movement command {} '.format(msg)
+        )
+
+        self.publisher_.publish(msg)
 
         self.i += 1
         self.i %= len(self.velocities)
